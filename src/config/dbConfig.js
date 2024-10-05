b44e865f6dc73e9fea0987f8484dab010b714b4d
shendi321 <roshanprajapati2004@gmail.com>
2024-10-05 05:31:44 +0530

    Connected to Mongodb

---
+++ src/config/dbConfig.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const serverConfig = require('./serverConfig');
+
+const connectDB = async () => {
+    try {
+        const { connection } = await mongoose.connect(serverConfig.DB_URL);
+        if(connection){
+            console.log(`Connected to MongDB at  ${connection.host}`);
+        }  
+    } catch (e) {
+        console.log(e);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
