7233f2cc24a565d123f352097f23b12dfe81836d
shendi321 <roshanprajapati2004@gmail.com>
2024-10-06 11:01:06 +0530

    Delete cartRoute.js file

--- src/schema/cartRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getCartById } = require('../controllers/cartController');
-
-const cartRouter = express.Router();
-
-cartRouter.get('/:id', getCartById);
-
-module.exports = cartRouter;
