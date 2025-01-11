41a2df0688ce0e76ef409c5f80f06c1d90277648
shendi321 <roshanprajapati2004@gmail.com>
2025-01-11 20:35:59 +0530

    Added code changes

--- src/config/mailConfig.js
+++ src/config/mailConfig.js
@@ -9,6 +9,7 @@ const {
 const transporter = nodemailer.createTransport({
   host: SMTP_HOST,
   port: SMTP_PORT,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: SMTP_USERNAME,
     pass: SMTP_PASSWORD
