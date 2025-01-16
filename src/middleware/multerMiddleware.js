3859d3cc3fcbd7fdc1781a4acfa6517125ad19b1
shendi321 <roshanprajapati2004@gmail.com>
2025-01-16 19:52:09 +0530

    Added code changes

--- src/middleware/multerMiddleware.js
+++ src/middleware/multerMiddleware.js
@@ -1,17 +1,31 @@
-const multer = require('multer');
-const path = require('path');
+import multer from 'multer';
+import path from 'path';
 
-const storageConfiguration = multer.diskStorage({
-  limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB file size limit
-  destination: (_req, file, next) => {
-    next(null, 'uploads/');
-  },
-  filename: (req, file, next) => {
-    console.log(file);
-    next(null, `${Date.now()}${path.extname(file.originalname)}`);
+const uploader = multer({
+  dest: 'uploads/',
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50 mb in size max limit
+  storage: multer.diskStorage({
+    destination: 'uploads/',
+    filename: (_req, file, next) => {
+      next(null, file.originalname);
+    }
+  }),
+  fileFilter: (_req, file, next) => {
+    let img = path.extname(file.originalname);
+
+    if (
+      img !== '.jpg' &&
+      img !== '.jpeg' &&
+      img !== '.webp' &&
+      img !== '.png' &&
+      img !== '.mp4'
+    ) {
+      next(new Error(`Unsupported file type! ${img}`), false);
+      return;
+    }
+
+    next(null, true);
   }
 });
 
-const uploader = multer({ storage: storageConfiguration });
-
-module.exports = uploader;
+export default uploader;
